cd73cb90a58490032f82c9a3f08faa0a4dc782c2
taomaofeng <taomaofeng@ygzykj.com>
2022-03-14 16:38:49 +0800

    ss

--- app/src/main/java/com/taomf/projectframe/MainActivity.java
+++ app/src/main/java/com/taomf/projectframe/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         int b = 3;
         int c = 4;
         int d = 5;
+        String ss = "taomf";
         int e = 6;
         int f = 7;
         int h = 8;
