681dd467dd84153bc1b13edadf65b0490454bc96
taomaofeng <taomaofeng@ygzykj.com>
2022-03-15 10:55:36 +0800

    '1

--- app/src/main/java/com/taomf/projectframe/MainActivity.java
+++ app/src/main/java/com/taomf/projectframe/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int a  = 2;
+        int b  = 2;
 
 
     }
